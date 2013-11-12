1359359ea4f1bfbd135cea9b1c54bf83ca63a680
Jim Lawruk <jimlawruk@yahoo.com>
2013-11-11 22:53:13 -0500

    video edit files

---
+++ Models/EditVideoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lawrukmvc.Models
+{
+    public class EditVideoViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+
+
+    }
+}
