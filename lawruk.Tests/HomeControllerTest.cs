12e9551c8b55f427c53c7261860aabcd78599964
Jim Lawruk <jimlawruk@yahoo.com>
2014-03-08 20:04:40 -0500

    Added lawruk.Test project

---
+++ lawruk.Tests/HomeControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using lawrukmvc.ViewModels;
+using lawrukmvc.Controllers;
+
+namespace lawruk.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void IndexViewModalHasButtons()
+        {
+            var homeController = new HomeController();
+            ViewResult viewResult = (ViewResult) homeController.Index();
+            HomeViewModel viewModel = (HomeViewModel)viewResult.Model;
+            Assert.IsTrue(viewModel.HomeButtons.Count > 5);
+        }
+    }
+}
