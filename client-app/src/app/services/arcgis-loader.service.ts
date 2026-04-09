c861b7508c819610c2dcb4707f49aa11c44f0b0c
Jim Lawruk <jimlawruk@yahoo.com>
2026-04-08 20:09:51 -0400

    Boston Marathon layer updates

--- client-app/src/app/services/arcgis-loader.service.ts
+++ client-app/src/app/services/arcgis-loader.service.ts
@@ -13,6 +13,7 @@ import type esriRequest from '@arcgis/core/request.js';
 import type BasemapToggle from '@arcgis/core/widgets/BasemapToggle.js';
 import type LayerList from '@arcgis/core/widgets/LayerList.js';
 import type Locate from '@arcgis/core/widgets/Locate.js';
+import type Expand from '@arcgis/core/widgets/Expand.js';
 
 declare const $arcgis: {
   import: <T = any>(modules: string | string[]) => Promise<T>;
@@ -32,6 +33,7 @@ export interface ArcGISClasses {
   BasemapToggle: typeof BasemapToggle;
   LayerList: typeof LayerList;
   Locate: typeof Locate;
+  Expand: typeof Expand;
   esriRequest: typeof esriRequest;
 }
 
@@ -58,6 +60,7 @@ export class ArcGISLoaderService {
         BasemapToggle,
         LayerList,
         Locate,
+        Expand,
       ] = await $arcgis.import([
         '@arcgis/core/Map.js',
         '@arcgis/core/views/MapView.js',
@@ -73,6 +76,7 @@ export class ArcGISLoaderService {
         '@arcgis/core/widgets/BasemapToggle.js',
         '@arcgis/core/widgets/LayerList.js',
         '@arcgis/core/widgets/Locate.js',
+        '@arcgis/core/widgets/Expand.js',
       ]);
 
       return {
@@ -90,6 +94,7 @@ export class ArcGISLoaderService {
         BasemapToggle,
         LayerList,
         Locate,
+        Expand,
       } as ArcGISClasses;
     });
 
